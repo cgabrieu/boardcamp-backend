80a8fca0d94c993cb330d2d26b830d707ac9edf0
Carlos Gabriel <carlosgabrielpc@live.com>
2021-10-17 01:37:02 -0300

    implementing categories methods

--- src/app.js
+++ src/app.js
@@ -1,13 +1,51 @@
 import express from 'express';
+import cors from 'cors';
 import connection from './database/database.js';
 
 const SERVER_PORT = 4000;
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
-app.get('/check-server', async (req, res) => {
-    res.status(200).send("Rodando plenamente ou talvez.");
+app.get('/check-server', (req, res) => {
+	res.status(200).send("Rodando plenamente ou talvez.");
+});
+
+//List Categories//
+app.get('/categories', async (req, res) => {
+	try {
+		const result = await connection.query("SELECT * FROM categories;");
+
+		if (result.rows.length === 0) {
+			res.send("Lista de categorias vazia.");
+			return;
+		}
+		res.status(200).send(result.rows);
+	} catch (error) {
+		console.log(error);
+		res.sendStatus(500);
+	}
+});
+
+//Insert Categories
+app.post('/categories', async (req, res) => {
+	try {
+		const categoryName = req.body.name;
+
+		if (!categoryName) return res.sendStatus(400);
+
+		const hasCategory = await connection.query("SELECT * FROM categories WHERE name = $1", [categoryName]);
+		
+		if (hasCategory.rowCount > 0) return res.statusCode(409);
+		
+		await connection.query("INSERT INTO categories (name) VALUES ($1)", [categoryName]);
+		res.sendStatus(201);
+	} catch (error) {
+		console.log(error);
+		res.sendStatus(500);
+	}
 });
 
 app.listen(SERVER_PORT, () => {
