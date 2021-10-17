2f036c4e69b2567dd9357d4d557d2861e2828bf8
Carlos Gabriel <carlosgabrielpc@live.com>
2021-10-17 11:49:46 -0300

    added get games method & post game rules

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import joi from 'joi;'
 import connection from './database/database.js';
 
 const SERVER_PORT = 4000;
@@ -8,6 +9,14 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const insertGameRules = joi.object({
+	name: joi.string().required(),
+	image: joi.string().uri().required().allow(''),
+	stockTotal: joi.number().min(1).required(),
+	categoryId: joi.number().required(),
+	pricePerDay: joi.number().min(1).required(),
+});
 
 app.get('/check-server', (req, res) => {
 	res.status(200).send("Rodando plenamente ou talvez.");
@@ -29,7 +38,7 @@ app.get('/categories', async (req, res) => {
 	}
 });
 
-//Insert Categories
+//Insert Category//
 app.post('/categories', async (req, res) => {
 	try {
 		const categoryName = req.body.name;
@@ -37,9 +46,52 @@ app.post('/categories', async (req, res) => {
 		if (!categoryName) return res.sendStatus(400);
 
 		const hasCategory = await connection.query("SELECT * FROM categories WHERE name = $1", [categoryName]);
-		
+
 		if (hasCategory.rowCount > 0) return res.statusCode(409);
-		
+
+		await connection.query("INSERT INTO categories (name) VALUES ($1)", [categoryName]);
+		res.sendStatus(201);
+	} catch (error) {
+		console.log(error);
+		res.sendStatus(500);
+	}
+});
+
+//List Games//
+app.get('/games', async (req, res) => {
+	try {
+		const { name } = req.query;
+		let result;
+		if (name) {
+			result = await connection
+				.query(`SELECT * FROM games WHERE name ILIKE $1`,
+					['%' + name + '%']);
+		} else {
+			result = await connection.query("SELECT * FROM games;");
+		}
+
+		if (result.rowCount === 0) {
+			res.send("Lista de jogos vazia.");
+			return;
+		}
+		res.status(200).send(result.rows);
+	} catch (error) {
+		console.log(error);
+		res.sendStatus(500);
+	}
+});
+
+//Insert Game//
+app.post('/games', async (req, res) => {
+	try {
+		const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
+
+		if (!gameName) return res.sendStatus(400);
+
+		const hasCategory = await connection.query("SELECT * FROM games WHERE name = $1", [gameName]);
+
+		if (hasCategory.rowCount > 0) return res.statusCode(409);
+
 		await connection.query("INSERT INTO categories (name) VALUES ($1)", [categoryName]);
 		res.sendStatus(201);
 	} catch (error) {
@@ -49,5 +101,5 @@ app.post('/categories', async (req, res) => {
 });
 
 app.listen(SERVER_PORT, () => {
-  console.log(`Server is listening on port ${SERVER_PORT}.`);
+	console.log(`Server is listening on port ${SERVER_PORT}.`);
 });
